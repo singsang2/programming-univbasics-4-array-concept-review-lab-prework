eb278273be49595a6693aecf49a2cf6e6f1580a0
Sung Bae <singsang2@gmail.com>
2020-03-31 12:19:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,16 +6,16 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.length.times do |index|
-    max |= array[index]
+  max |= array[0]
+  (1...array.length).each do |index|
     max = array[index] if max < array[index]
   end
   max
 end
 
 def find_min_value(array)
-  array.length.times do |index|
-    min |= array[index]
+  min |= array[0]
+  (1...array.length).each do |index|
     min = array[index] if min > array[index]
   end
   min
